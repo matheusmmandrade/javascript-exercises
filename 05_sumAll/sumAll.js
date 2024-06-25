2234194b6946362c4cec49cc7c863d473a9c05bc
Matheus <matheusmmandrade@gmail.com>
2024-06-25 07:37:57 -0300

    vs 01

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
+
+    if(firstNumber < 0  || typeof(firstNumber) !== 'number' || typeof(lastNumber) !== 'number') return "ERROR";
+
+    let lowerNumber = Math.min(firstNumber, lastNumber);
+    let biggerNumber = Math.max(firstNumber, lastNumber);
+
+    let sum = 0;
+    for(let i = lowerNumber; i <= biggerNumber; i++){
+        sum = sum + i;
+    }
+    return sum;
 
 };
 
